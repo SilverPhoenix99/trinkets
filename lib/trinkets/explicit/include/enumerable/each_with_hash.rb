38b98a72ac049c5384db818abc7d62164f4cb525
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2024-02-03 19:57:39 +0000

    Fixed Enumerator::with_hash.

--- lib/trinkets/explicit/include/enumerable/each_with_hash.rb
+++ lib/trinkets/explicit/include/enumerable/each_with_hash.rb
@@ -2,16 +2,13 @@ module Trinkets
 
   module Enumerable
     module WithHash
-      def each_with_hash(&)
-        return enum_for(:each_with_hash) unless block_given?
-        each_with_object({}, &)
-      end
+      def each_with_hash(&) = each_with_object({}, &)
     end
   end
 
   module Enumerator
     module WithHash
-      alias_method :with_hash, :each_with_hash
+      def with_hash(&) = each_with_hash(&)
     end
   end
 end
