7cdde1f6d0d69a0dc905f1c003fb0c831d0edaa5
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2024-02-11 20:11:27 +0000

    Refactor.
    Co-authored-by: P3t3rU5 <pedro.at.miranda@gmail.com>

--- lib/explicit/trinkets/extend/class/init.rb
+++ lib/explicit/trinkets/extend/class/init.rb
@@ -5,7 +5,84 @@ module Trinkets
     module Init
       ATTR = %i[accessor reader writer none].freeze
       Attribute = Struct.new(:name, :attr, :kw, keyword_init: true)
-      Parameters = Struct.new(:req, :key_req, :key_opt, keyword_init: true)
+
+      # @!attribute r req
+      #   @return [Hash[Symbol, Attribute]]
+      # @!attribute r key_req
+      #   @return [Hash[Symbol, Attribute]]
+      # @!attribute r key_opt
+      #   @return [Hash[Symbol, Attribute]]
+      class Parameters < Struct.new(:req, :key_req, :key_opt, keyword_init: true)
+
+        def bind(values, kw_values)
+          validate values, kw_values
+
+          req = self.req.values.zip(values).each_with_object({}) do |(param, value), h|
+            h[param.name] = BoundAttribute.new(
+              name: param.name,
+              attr: param.attr,
+              kw: param.kw,
+              value:
+            )
+          end
+
+          key_req = self.key_req.values.each_with_object({}) do |param, h|
+            h[param.name] = BoundAttribute.new(
+              name: param.name,
+              attr: param.attr,
+              kw: param.kw,
+              value: kw_values[param.name]
+            )
+          end
+
+          key_opt = self.key_opt.values.each_with_object({}) do |param, h|
+            key, value = kw_values.assoc(param.name)
+            h[param.name] = BoundAttribute.new(
+              name: param.name,
+              attr: param.attr,
+              kw: param.kw,
+              value: key ? value : param.kw[:default]
+            )
+          end
+
+          Parameters.new(req:, key_req:, key_opt:)
+
+        end
+
+        def all = req.merge(all_keys)
+
+        def all_keys = key_req.merge(key_opt)
+
+        private def validate(values, kw_values)
+
+          unless req.size == values.size
+            raise ArgumentError, "wrong number of arguments (given #{values.size}, expected #{req.size})"
+          end
+
+          missing_keys = key_req.keys - kw_values.keys
+          unless missing_keys.empty?
+            missing_keys = missing_keys.map(&:inspect).join(', ')
+            raise ArgumentError, "missing keywords: #{missing_keys}"
+          end
+
+          unknown_keywords = kw_values.except(*key_req.keys, *key_opt.keys)
+          unless unknown_keywords.empty?
+            unknown_keywords = unknown_keywords.keys.map(&:to_sym).map(&:inspect).join(', ')
+            raise ArgumentError, "unknown keywords: #{unknown_keywords}"
+          end
+
+        end
+      end
+
+      class BoundAttribute < Attribute
+
+        attr_reader :value
+
+        def initialize(name:, attr:, kw:, value:)
+          super(name:, attr:, kw:)
+          @value = value
+        end
+      end
 
       def init(*attrs, attr: ATTR.first, kw: false)
         attrs = Init.send(:sanitize_attrs, attrs, attr:, kw:)
@@ -27,16 +104,19 @@ module Trinkets
         attrs.each { |param| attr_init.call(param.name, attr: param.attr, kw: param.kw) }
 
         # hash with 3 keys: {
-        #   FalseClass => []  # positional args
-        #   TrueClass  => []  # mandatory kw args
-        #   Hash       => []  # optional kw args with default value
+        #   FalseClass => { :name => Attribute }  # positional args
+        #   TrueClass  => { :name => Attribute }  # mandatory kw args
+        #   Hash       => { :name => Attribute }  # optional kw args with default value
         # }
-        grouped_params = attrs.group_by { |param| param.kw.class }
+        grouped_attrs = attrs.group_by { |param| param.kw.class }
+          .transform_values! do |params|
+            params.each_with_object({}) { |p, h| h[p.name] = p }
+          end
 
         params = Parameters.new(
-          req:     grouped_params[FalseClass] || [],
-          key_req: grouped_params[TrueClass]  || [],
-          key_opt: grouped_params[Hash]       || []
+          req:     grouped_attrs[FalseClass] || {},
+          key_req: grouped_attrs[TrueClass]  || {},
+          key_opt: grouped_attrs[Hash]       || {}
         )
 
         init_method = Init.send(:define_initialize, params)
@@ -81,44 +161,12 @@ module Trinkets
         private def define_initialize(params)
           ->(*values, **kw_values) do
 
-            ::Trinkets::Class::Init.send :validate_parameters, params, values, kw_values
+            params = params.bind(values, kw_values)
 
-            params.req.zip(values).each do |param, value|
-              instance_variable_set "@#{param.name}", value
-            end
-
-            params.key_req.each do |param|
-              instance_variable_set "@#{param.name}", kw_values[param.name]
-            end
-
-            params.key_opt.each do |param|
-              key, value = kw_values.assoc(param.name)
-              value = param.kw[:default] unless key
-              instance_variable_set "@#{param.name}", value
+            params.all.each do |name, param|
+              instance_variable_set "@#{name}", param.value
             end
           end
-        end
-
-        private def validate_parameters(params, values, kw_values)
-
-          unless params.req.size == values.size
-            raise ArgumentError, "wrong number of arguments (given #{values.size}, expected #{params.req.size})"
-          end
-
-          key_req = params.key_req.map(&:name)
-          missing_keys = key_req - kw_values.keys
-          unless missing_keys.empty?
-            missing_keys = missing_keys.map(&:inspect).join(', ')
-            raise ArgumentError, "missing keywords: #{missing_keys}"
-          end
-
-          key_opt = params.key_opt.map(&:name)
-          unknown_keywords = kw_values.except(*key_req, *key_opt)
-          unless unknown_keywords.empty?
-            unknown_keywords = unknown_keywords.keys.map(&:to_sym).map(&:inspect).join(', ')
-            raise ArgumentError, "unknown keywords: #{unknown_keywords}"
-          end
-
         end
       end
     end
